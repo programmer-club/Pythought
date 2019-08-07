b235f6c102b005b033bb67df456951f0e8016c00
Isaac <48636131+Moogamouth@users.noreply.github.com>
2019-08-07 12:33:51 -0700

    Update and rename pythought.py to Pythought.py

---
+++ Pythought.py
@@ -0,0 +1,13 @@
+abclen = 1
+abcnum = 0
+abc = abcdefghijklmnopqrstuvwxyz
+def var():
+  if abcnum = 26:
+    abclen =+ 1
+  abcnum =+ 1
+  X = abclen - 1
+  while not X = 0:
+    var_name =+ "X"
+  var_name =+ abc[abcnum]
+  string(var_name) = null  
+  
